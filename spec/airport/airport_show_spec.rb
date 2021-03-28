d2d5952852ef681d7b104f9ccca63e813d9d3167
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-03-27 21:14:38 -0600

    Add functionality for User Story 5; test passing.

--- spec/airport/airport_show_spec.rb
+++ spec/airport/airport_show_spec.rb
@@ -10,3 +10,19 @@ RSpec.describe "Story2: When I visit airport details page", type: :feature do
   expect(page).to have_content(jfk.international_hub)
   end
 end
+
+RSpec.describe "Story5: When I visit an airport's available airlines page", type: :feature do
+  it "displays airlines at specified airport and airline details" do
+    jfk = Airport.create!(name: 'John F Kennedy', terminals: 175, international_hub: true)
+    spirit = jfk.airlines.create!(name:'Spirit', flight_number: 424, domestic_flight: true)
+    united = jfk.airlines.create!(name:'United', flight_number: 513, domestic_flight: true)
+
+  visit "/airports/airports/#{jfk.id}/airlines"
+  expect(page).to have_content(spirit.name)
+  expect(page).to have_content(spirit.flight_number)
+  expect(page).to have_content(spirit.domestic_flight)
+  expect(page).to have_content(united.name)
+  expect(page).to have_content(united.flight_number)
+  expect(page).to have_content(united.domestic_flight)
+  end
+end
