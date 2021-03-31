32594e5217898aeaace4b442c2e847ecad0a0811
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-03-31 08:59:51 -0600

    Add new and create to airport_airlines; test passing. User story 13 complete.

--- app/controllers/airport_airlines_controller.rb
+++ app/controllers/airport_airlines_controller.rb
@@ -3,4 +3,20 @@ class AirportAirlinesController < ApplicationController
     @airport = Airport.find(params[:id])
     @airlines = @airport.airlines
   end
+
+  def new
+    @airport = Airport.find(params[:id])
+    @airlines = @airport.airlines
+  end
+
+  def create
+    @airport = Airport.find(params[:id])
+    @airport.airlines.create!(airline_params)
+
+    redirect_to "/airports/airports/#{@airport.id}/airlines"
+  end
+
+  def airline_params
+    params.permit(:name, :flight_number, :domestic_flight)
+  end
 end
--- spec/features/airport_airlines/new_spec.rb
+++ spec/features/airport_airlines/new_spec.rb
@@ -5,19 +5,21 @@ RSpec.describe "Story13: When I visit airlines at airport index page", type: :fe
     jfk = Airport.create!(name: 'John F Kennedy', terminals: 6, international_hub: true)
     jfk.airlines.create!(name:'Delta', flight_number: 717, domestic_flight: true)
     jfk.airlines.create!(name:'United', flight_number: 431, domestic_flight: false)
-    jfk.airlines.create!(name:'American', flight_number: 414, domestic_flight: true)
 
-    visit "/airports/airports/#{jfk.id}"
+    visit "/airports/airports/#{jfk.id}/airlines"
 
     click_link 'Add Airline'
 
     expect(current_path).to eq("/airports/airports/#{jfk.id}/airlines/new")
 
     fill_in 'Name', with: 'American Airlines'
-    fill_in 'Domestic Flight', with: 'true'
+    fill_in 'flight_number', with: 414
+    fill_in 'domestic_flight', with: 'true'
     click_on 'Add Airline'
 
-    expect(current_path).to eq("/airports/airports/#{jfk.id}")
+    expect(current_path).to eq("/airports/airports/#{jfk.id}/airlines")
     expect(page).to have_content('American Airlines')
+    expect(page).to have_content(414)
+    expect(page).to have_content('true')
   end
 end
